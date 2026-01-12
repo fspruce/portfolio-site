174b05ce5c097efca377bb63053f8911019353d9
fspruce <f.spruce@proton.me>
2026-01-12 17:53:50 +0000

    Restyle scroll to top button, implement on projects page

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -2,15 +2,32 @@ import { useEffect, useState } from "react";
 
 export default function ScrollToTop() {
   const [hasScrolled, setHasScrolled] = useState(false);
+  const [buttonBottom, setButtonBottom] = useState("20px");
 
   useEffect(() => {
     const handleScroll = () => {
       const y = window.scrollY;
       setHasScrolled(y !== 0);
+
+      const footer = document.querySelector("footer");
+      if (footer) {
+        const footerTop = footer.getBoundingClientRect().top + window.scrollY;
+        const viewportHeight = window.innerHeight;
+        const buttonMargin = 10; // Approximate height of the button
+
+        if (window.scrollY + viewportHeight >= footerTop) {
+          setButtonBottom(
+            `${viewportHeight - (footerTop - window.scrollY) + buttonMargin}px`
+          );
+        } else {
+          setButtonBottom("20px");
+        }
+      }
     };
+
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [hasScrolled]);
+  }, []);
 
   function handleScrollButton() {
     window.scrollTo(0, 0);
@@ -20,9 +37,11 @@ export default function ScrollToTop() {
   return (
     <button
       onClick={() => handleScrollButton()}
-      className={`cursor-pointer fixed bottom-5 left-1/2 transform -translate-x-1/2 px-4 py-2 rounded shadow-lg z-50 bg-base-300/20 ${!hasScrolled && "hidden"}`}
+      style={{ bottom: buttonBottom }}
+      className={`cursor-pointer fixed transform px-4 py-2 rounded border border-accent/20 shadow-lg z-50 bg-base-100/40 md:max-w-[30%] lg:max-w-[10%] ${!hasScrolled && "hidden"} right-5 sm:left-1/2 sm:-translate-x-1/2`}
     >
-      Scroll to Top
+      <span className="md:hidden">↑</span>
+      <span className="hidden md:block">Scroll to Top</span>
     </button>
   );
 }
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
 import ProjectCards from "../components/ProjectCards";
 import ProjectCardsSkeleton from "../components/ProjectCardsSkeleton";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function ProjectsPage() {
   const [projects, setProjects] = useState([]);
@@ -59,6 +60,7 @@ export default function ProjectsPage() {
             liveLink={project.live_link}
           />
         ))}
+        <ScrollToTop />
       </div>
     );
   }
